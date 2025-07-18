fd8c3b35c53215d5182c03218942ed555758daf7
ravichandranu <root.ravi92@gmail.com>
2025-07-18 12:07:51 +0530

    msg

--- src/App.java
+++ src/App.java
@@ -3,6 +3,6 @@ public class App
     public static void main(String[] args) throws Exception
      {
        System.out.println("Welcome to Trichy");
-       System.out.println("Hello");
+      
     }
 }
