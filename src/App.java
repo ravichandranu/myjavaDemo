682d6f008dce4c53e0f188dcdf1ac36242f7a1d5
ArunRamesh54 <arun110504@gmail.com>
2025-07-18 12:40:50 +0530

    Update App.java

--- src/App.java
+++ src/App.java
@@ -3,6 +3,7 @@ public class App
     public static void main(String[] args) throws Exception
      {
        System.out.println("Welcome to Trichy");
+         System.out.println("Welcome to chennai");
       
     }
 }
